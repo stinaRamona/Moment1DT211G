cdfd44a84d0902dc8ca6bd076258f8f18db63b9b
Stina Persson <stpe1901@student.miun.se>
2024-01-19 13:55:40 +0100

    webbplatsen har optimerats via npm run build

---
+++ dist/index.cae257d3.js
@@ -0,0 +1,2 @@
+document.getElementById("toggle_darkmode").addEventListener("click",function(){document.getElementsByTagName("main")[0].className="dark_mode"});
+//# sourceMappingURL=index.cae257d3.js.map
