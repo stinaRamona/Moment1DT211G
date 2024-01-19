bff318a99459d6bd4edf2d8e1de1d4ddecac8a60
Stina Persson <stpe1901@student.miun.se>
2024-01-19 12:14:32 +0100

    Lagt till funktionalitet i knapp för dark mode som Javascript för sidan

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,14 @@
 "use strict"; 
 
+/*Hämtar namnet på knappen */
 let toggleBtn = document.getElementById("toggle_darkmode"); 
 
+/*Lägger till eventlyssnare på click som kallar på funktion */
 toggleBtn.addEventListener('click', toggleFunction);
 
-function toggleFunction(){
-    console.log("hej"); 
+/*Funktionen hämtar "main" via taggnamn och jag ger den klassen dark_mode*/
+function toggleFunction(){ 
+    let mainEl = document.getElementsByTagName('main')[0]; 
+
+    mainEl.className = "dark_mode"; 
 }
